9f572afa4b5c39f3d98eea14c893217fd01fca71
VoldemarM <aniairina@gmail.com>
2023-09-14 13:33:42 +0300

    change code1

--- src/main/java/ru/geekbrains/lesson6/srp2/Order.java
+++ src/main/java/ru/geekbrains/lesson6/srp2/Order.java
@@ -13,18 +13,6 @@ public class Order {
     private  int price;
     public Order(){
     }
-//      public void inputFromConsole(){
-//        clientName = prompt("Client name: ");
-//        product = prompt("Product: ");
-//        qnt = Integer.parseInt(prompt("Quantity: "));
-//        price = Integer.parseInt(prompt("Price: "));
-//    }
-//    private String prompt(String message) {
-//        Scanner in = new Scanner(System.in);
-//        System.out.print(message);
-//        return in.nextLine();
-//    }
-
 
     public void setClientName(String clientName) {
         this.clientName = clientName;
